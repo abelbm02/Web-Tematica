5c52699a12db008a867fa931854420deebd64aed
Abel Blanco Muñoz <abelblanco447@gmail.com>
2025-05-21 18:25:26 +0200

    sonido y boton con brode a todas las pags

--- js/cuestionariologica.js
+++ js/cuestionariologica.js
@@ -108,3 +108,19 @@ const quizData = [
   document.getElementById('toggleSidebar').addEventListener('click', function() {
     document.getElementById('sidebarMenu').classList.toggle('show');
   });
+
+  function setupSidebarToggle() {
+    const toggleButton = document.getElementById('toggleSidebar');
+    const audio = document.getElementById('mcqueenaudio');
+    const sidebar = document.getElementById('sidebar');
+
+    toggleButton.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+
+      sidebar.classList.toggle('visible');
+    });
+  }
+
+ 
+  document.addEventListener('DOMContentLoaded', setupSidebarToggle);
